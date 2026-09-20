b4388b1e2ee2402e4e512ce932d0391aa0ee151e
jiyad shaikh <jiyads919@gmail.com>
2026-09-21 00:49:21 +0530

    Connect frontend to deployed backend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://projectly-e8ak.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
